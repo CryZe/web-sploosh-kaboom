86782df35a7352790b10684876c4e1dab456e873
Christopher Serr <christopher.serr@gmail.com>
2020-04-17 19:38:06 +0200

    gh-pages

---
+++ docs/precache-manifest.588f83c8411d6eb55bda0a98eebeeff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d9c465a40ef912d738cc7f158ca411",
+    "url": "/web-sploosh-kaboom/index.html"
+  },
+  {
+    "revision": "be481680541e25f97b3c",
+    "url": "/web-sploosh-kaboom/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "37ba6855455b1cf3ec4a",
+    "url": "/web-sploosh-kaboom/static/js/2.919689a1.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/web-sploosh-kaboom/static/js/2.919689a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be481680541e25f97b3c",
+    "url": "/web-sploosh-kaboom/static/js/main.a71fee3a.chunk.js"
+  },
+  {
+    "revision": "5333a43730c9543f3258",
+    "url": "/web-sploosh-kaboom/static/js/runtime-main.d680b619.js"
+  }
+]);
